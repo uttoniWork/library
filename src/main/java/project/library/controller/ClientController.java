271de02d3a6eb8dc454a077536d41b49ff4802d5
GustavoRT-debug <gustavo_ramos@gec.inatel.br>
2023-04-16 16:35:46 -0300

    LP-2 Refatorando client controller para usar anotações do Spring

--- src/main/java/project/library/controller/ClientController.java
+++ src/main/java/project/library/controller/ClientController.java
@@ -1,12 +1,17 @@
 package project.library.controller;
 
-import org.example.dto.ClientLoginRequest;
-import org.example.dto.ClientRequest;
-import org.example.model.Client;
-import org.example.service.ClientService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import project.library.dto.ClientLoginRequest;
+import project.library.dto.ClientRequest;
+import project.library.model.Client;
+import project.library.service.ClientService;
 
-//@RestController
-//@RequestMapping("/client")
+@RestController
+@RequestMapping("/client")
 public class ClientController {
 
     private final ClientService clientService;
@@ -15,17 +20,14 @@ public class ClientController {
         this.clientService = clientService;
     }
 
-    //    @PostMapping
-//    public ResponseEntity<Client> postClient(@RequestBody ClientRequest clientRequest){
-    public Client postClient(ClientRequest clientRequest){
+    @PostMapping
+    public ResponseEntity<Client> postClient(@RequestBody ClientRequest clientRequest) {
         Client savedClient = clientService.saveClient(clientRequest);
 
-//        return ResponseEntity.ok(savedClient);
-        return savedClient;
+        return ResponseEntity.ok(savedClient);
     }
 
-//    public ResponseEntity<Long> loginClient(@RequestBody ClientLoginRequest clientLoginRequest){
-    public Long loginClient(ClientLoginRequest clientLoginRequest){
+    public ResponseEntity<Long> loginClient(@RequestBody ClientLoginRequest clientLoginRequest) {
         Long clientId = clientService.loginClient(clientLoginRequest);
 
         return clientId;
