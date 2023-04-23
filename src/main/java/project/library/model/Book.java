251422cb5dbcbda1b46a333f87fa98cd644dca17
uttoniWork <uttoni.brandani@ges.inatel.br>
2023-04-22 22:02:31 -0300

    LP-4 Adicionando construtor vazio, getters e setters às classes de modelagem

--- src/main/java/project/library/model/Book.java
+++ src/main/java/project/library/model/Book.java
@@ -15,18 +15,20 @@ public class Book {
     @GeneratedValue
     private Long bookId;
     @Property
-    private final String title;
+    private String title;
     @Property
-    private final String author;
+    private String author;
     @Property
-    private final String coverImage;
-    private final List<String> genres;
+    private String coverImage;
+    private List<String> genres;
     @Property
-    private final String editor;
+    private String editor;
     @Property
-    private final Short releaseYear;
+    private Short releaseYear;
     @Property
-    private final LocalDateTime dateCreated;
+    private LocalDateTime dateCreated;
+
+    public Book() {}
 
     public Book(String title, String author, String coverImage, List<String> genres, String editor, Short releaseYear) {
         this.title = title;
@@ -42,27 +44,63 @@ public class Book {
         return bookId;
     }
 
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
     public String getTitle() {
         return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getAuthor() {
         return author;
     }
 
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
     public String getCoverImage() {
         return coverImage;
+    }
+
+    public void setCoverImage(String coverImage) {
+        this.coverImage = coverImage;
     }
 
     public List<String> getGenres() {
         return genres;
     }
 
+    public void setGenres(List<String> genres) {
+        this.genres = genres;
+    }
+
     public String getEditor() {
         return editor;
     }
 
+    public void setEditor(String editor) {
+        this.editor = editor;
+    }
+
     public Short getReleaseYear() {
         return releaseYear;
+    }
+
+    public void setReleaseYear(Short releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDateTime dateCreated) {
+        this.dateCreated = dateCreated;
     }
 }
