def43fa40964a30828d7e4284cf6565da3d74fc2
Vinícius Ribas <vinicius.ribas@sensedia.com>
2023-05-12 01:01:52 -0300

    LP-8 config do frontend

--- src/front/ts/app.js
+++ src/front/ts/app.js
@@ -3,6 +3,41 @@ var btnSignup = document.querySelector("#signup");
 var body = document.querySelector("body");
 var element = document.getElementById("content");
 
+//////////////////////////////////////////////
+var berserkerDiv = document.querySelector("#berserkerDiv");
+var griffithDiv = document.querySelector("#griffithDiv");
+berserkerDiv.addEventListener("click", function () {
+
+    griffithDiv.style.display = 'block';
+});
+griffithDiv.addEventListener("click", function () {
+    griffithDiv.style.display = 'none';
+});
+
+//////////////////////////////////////////////
+var berserkerDiv = document.querySelector("#berserkerDiv");
+var griffithDiv = document.querySelector("#griffithDiv");
+berserkerDiv.addEventListener("click", function () {
+
+    griffithDiv.style.display = 'block';
+});
+griffithDiv.addEventListener("click", function () {
+    griffithDiv.style.display = 'none';
+});
+
+///////////////////////////////////////////////
+
+function showPopup(img) {
+    const popup = document.getElementById('popup');
+    const popupImage = document.getElementById('popup-image');
+    const popupTitle = document.getElementById('popup-title');
+
+    popupImage.src = img.src;
+    popupTitle.innerText = img.alt;
+    popup.style.display = 'block';
+
+
+}
 var loggedClient = {
     clientId: "",
     username: ""
@@ -16,135 +51,128 @@ btnSignup.addEventListener("click", function () {
     body.className = "sign-up-js";
 });
 
+
+function getClient(){
+    return localStorage.getItem("storageLoggedClient")
+}
+function saveClient(client){
+    localStorage.setItem("storageLoggedClient", client);
+}
+
+
 function HideShow(id, inverse_id){
     document.getElementById(id).style.display = 'block';
     document.getElementById(inverse_id).style.display = 'none';
 }
 
-function showPopup(img) {
-    const popup = document.getElementById('popup');
-    const popupImage = document.getElementById('popup-image');
-    const popupTitle = document.getElementById('popup-title');
 
-    popupImage.src = img.src;
-    popupTitle.innerText = img.alt;
+function showRegister(){
+    document.getElementById('content').style.display = 'block'
 
-    popup.style.display = 'flex';
-  }
+    // console.log("criando livro")
 
-  function closePopup() {
-    const popup = document.getElementById('popup');
+    // // document.getElementById(idDiv).style.display = 'none'
 
-    popup.style.display = 'none';
-  }
+    // // loggedClient = localStorage.getItem("storageLoggedClient");
 
-function showRegister(id){
-    document.getElementById(id).style.display = 'block'
+    // let url = 'http://localhost:15000/book';
 
-    console.log("criando livro")
+    // console.log("url: " + url)
 
-    // document.getElementById(idDiv).style.display = 'none'
+    // var form = document.getElementById("createBook");
+
+    // console.log("Form: " + form)
+    // form === null || form === void 0 ? void 0 : form.addEventListener("submit",function (event) {
+    //     event.preventDefault();
+
+    //     console.log("entrei no form")
+
+    //     const clientId = 2
+    //     const title = document.getElementById("book_name").value
+    //     const author = document.getElementById("autor").value
+    //     // const coverImage = document.getElementById("image").value
+    //     const coverImage = "foto"
+    //     // const genres = document.getElementById("genero").value
+    //     const genres = ["Fantasia"]
+    //     const editor = document.getElementById("editora").value
+    //     // const releaseYear = document.getElementById("mes").value
+    //     const releaseYear = 2023
+
+    //     var bookRequest = {
+    //         "clientId": clientId,
+    //         "title": title,
+    //         "author": author,
+    //         "coverImage": coverImage,
+    //         "genres": genres,
+    //         "editor": editor,
+    //         "releaseYear": releaseYear
+    //     };
+
+    //     console.log("request enviado: " + JSON.stringify(bookRequest))
+
+    //     fetch(url, {
+    //         method: 'POST',
+    //         headers: {
+    //             // 'Accept': 'application/json',
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify(bookRequest)
+    //     })
+    //         .then(response => {
+    //             console.log(response.json())
+    //         })
+    // });
+}
+
+function createBook(){
+
+    // console.log("criando livro")
+
+     document.getElementById('content').style.display = 'none'
 
     // loggedClient = localStorage.getItem("storageLoggedClient");
 
-    let url = 'http://localhost:15000/book';
+    // let url = 'http://localhost:15000/book';
 
-    console.log("url: " + url)
+    // var form = document.getElementById("createBook");
+    // form === null || form === void 0 ? void 0 : form.addEventListener("submit", function (event) {
+    //     event.preventDefault();
 
-    var form = document.getElementById("createBook");
+    //     const clientId = loggedClient.clientId
+    //     const title = document.getElementById("book_name").value
+    //     const author = document.getElementById("autor").value
+    //     // const coverImage = document.getElementById("image").value
+    //     const coverImage = "foto"
+    //     // const genres = document.getElementById("genero").value
+    //     const genres = ["Fantasia"]
+    //     const editor = document.getElementById("editora").value
+    //     // const releaseYear = document.getElementById("mes").value
+    //     const releaseYear = 2023
 
-    console.log("Form: " + form)
-    form === null || form === void 0 ? void 0 : form.addEventListener("submit",function (event) {
-        event.preventDefault();
+    //     var bookRequest = {
+    //       "clientId": clientId,
+    //       "title": title,
+    //       "author": author,
+    //       "coverImage": coverImage,
+    //       "genres": genres,
+    //       "editor": editor,
+    //       "releaseYear": releaseYear
+    //     };
 
-        console.log("entrei no form")
+    //     console.log(JSON.stringify(bookRequest))
 
-        const clientId = 2
-        const title = document.getElementById("book_name").value
-        const author = document.getElementById("autor").value
-        // const coverImage = document.getElementById("image").value
-        const coverImage = "foto"
-        // const genres = document.getElementById("genero").value
-        const genres = ["Fantasia"]
-        const editor = document.getElementById("editora").value
-        // const releaseYear = document.getElementById("mes").value
-        const releaseYear = 2023
-
-        var bookRequest = {
-            "clientId": clientId,
-            "title": title,
-            "author": author,
-            "coverImage": coverImage,
-            "genres": genres,
-            "editor": editor,
-            "releaseYear": releaseYear
-        };
-
-        console.log("request enviado: " + JSON.stringify(bookRequest))
-
-        fetch(url, {
-            method: 'POST',
-            headers: {
-                // 'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(bookRequest)
-        })
-            .then(response => {
-                console.log(response.json())
-            })
-    });
-}
-
-function createBook(idDiv){
-
-    console.log("criando livro")
-
-    // document.getElementById(idDiv).style.display = 'none'
-
-    loggedClient = localStorage.getItem("storageLoggedClient");
-
-    let url = 'http://localhost:15000/book';
-
-    var form = document.getElementById("createBook");
-    form === null || form === void 0 ? void 0 : form.addEventListener("submit", function (event) {
-        event.preventDefault();
-
-        const clientId = loggedClient.clientId
-        const title = document.getElementById("book_name").value
-        const author = document.getElementById("autor").value
-        // const coverImage = document.getElementById("image").value
-        const coverImage = "foto"
-        // const genres = document.getElementById("genero").value
-        const genres = ["Fantasia"]
-        const editor = document.getElementById("editora").value
-        // const releaseYear = document.getElementById("mes").value
-        const releaseYear = 2023
-
-        var bookRequest = {
-          "clientId": clientId,
-          "title": title,
-          "author": author,
-          "coverImage": coverImage,
-          "genres": genres,
-          "editor": editor,
-          "releaseYear": releaseYear
-        };
-
-        console.log(JSON.stringify(bookRequest))
-
-        fetch(url, {
-            method: 'POST',
-            headers: {
-                // 'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(bookRequest)
-        })
-            .then(response => {
-                console.log(response.json())
-            })
-    });
+    //     fetch(url, {
+    //         method: 'POST',
+    //         headers: {
+    //             // 'Accept': 'application/json',
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify(bookRequest)
+    //     })
+    //         .then(response => {
+    //             console.log(response.json())
+    //         })
+    // });
 }
 
 function login() {
@@ -167,16 +195,14 @@ function login() {
         })
             .then(response =>  response.json())
             .then((json) => {
-                // localStorage.setItem("storageLoggedClient", loggedClient);
                 loggedClient = json
+                saveClient(loggedClient);
+                document.getElementById('clientName').textContent = loggedClient.username;
                 window.open('./index.html');
                 window.close();
             })
     });
 
-
-    window.open('./index.html');
-    window.close();
 }
 
 function create() {
@@ -209,3 +235,5 @@ function create() {
     });
     alert("usuario cadastrado");
 }
+
+
