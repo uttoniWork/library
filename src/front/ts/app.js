e04cbee4c52bf862f3df983f933efd18e78a7396
vinicius <vinicius_batista@gec.inatel.br>
2023-05-26 20:12:47 -0300

    implementação de test do front

--- src/front/ts/app.js
+++ src/front/ts/app.js
@@ -1,30 +1,39 @@
-var btnSignin = document.querySelector("#signin");
-var btnSignup = document.querySelector("#signup");
-var body = document.querySelector("body");
-var element = document.getElementById("content");
 
-var loggedClient = {
-    clientId: "",
-    userName: ""
-};
+function getItem(){
+    return localStorage.getItem(client)
 
-var outroClient;
+    window.onload
+}
 
-btnSignin.addEventListener("click", function () {
-    body.className = "sign-in-js";
-});
-
-btnSignup.addEventListener("click", function () {
-    body.className = "sign-up-js";
-});
 
 function HideShow(id, inverse_id){
     document.getElementById(id).style.display = 'block';
     document.getElementById(inverse_id).style.display = 'none';
 }
 
+//////////////////////////   DON'T TOUCH IS WORKING BY MIRACLE           /////////////////////////////////////////////////////
+var image = document.querySelector(".berserkerClass");
+var popup = document.querySelector(".popup");
+image.addEventListener("click", function () {
+
+    popup.style.display = 'block';
+});
+popup.addEventListener("click", function () {
+    popup.style.display = 'none';
+});
+////////////////////////////////////////////////////////////////////////////////
+var imageList = document.querySelector(".berserkerClassList");
+var popupList = document.querySelector(".popupList");
+popupList.addEventListener("click", function () {
+
+    popupList.style.display = 'block';
+});
+popupList.addEventListener("click", function () {
+    popupList.style.display = 'none';
+});
+////////////////////////////////////////////////////////////////////////////////////
 function showPopup(img) {
-    const popup = document.getElementById('popup');
+    const popup = document.getElementById('popupId');
     const popupImage = document.getElementById('popup-image');
     const popupTitle = document.getElementById('popup-title');
 
@@ -35,31 +44,62 @@ function showPopup(img) {
   }
 
   function closePopup() {
-    const popup = document.getElementById('popup');
+    const popup = document.getElementById('popupId');
 
     popup.style.display = 'none';
   }
 
+  function showPopupList(img) {
+    const popup = document.getElementById('popupListId');
+    const popupImage = document.getElementById('popup-image-list');
+    const popupTitle = document.getElementById('popup-title-list');
 
+    popupImage.src = img.src;
+    popupTitle.innerText = img.alt;
 
+    popup.style.display = 'flex';
+  }
+
+  function closePopup() {
+    const popup = document.getElementById('popupListId');
+
+    popup.style.display = 'none';
+  }
+
+////////////////////////////////////////////////////////////////////////////////////////////////
 
 function showRegister(id){
-    document.getElementById(id).style.display = 'block'
+    document.getElementById(id).style.display = 'block';
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////
+function searchBooks(idDiv){
+    var campo = document.getElementById("text-search");
+    if (campo.value !== "") {
+        document.getElementById(idDiv).style.display = 'block';
+    }
+    else{
+        window.alert("Preencha o campo de pesquisa")
+    }
 }
 
+function validaCampo(){
+    var title = document.getElementById("book_name")
+    var author = document.getElementById("autor")
+    var coverImage = document.getElementById("image")
+    var gender = document.getElementById("genero")
+    var editor = document.getElementById("editora")
+    var releaseYear = document.getElementById("mes")
+
+    if (title.value !== '' && author.value !== '' && coverImage.value !== '' && gender.value !== '' && editor.value !== '' && releaseYear.value !== ''){
+        createBook();
+        document.getElementById('content').style.display = 'none';
+    }
+}
+
+function createBook(){
 
 
-
-
-
-
-
-
-function createBook(id){
-
-    document.getElementById(id).style.display = 'none'
-
-    let url = 'http://localhost:15000/book';
+    /*let url = 'http://localhost:15000/book';
 
     var form = document.getElementById("create");
     form === null || form === void 0 ? void 0 : form.addEventListener("submit", function (event) {
@@ -77,6 +117,7 @@ function createBook(id){
         var clientRequest = {
           "clientId": clientId,
           "title": title,
+          "author": author,
           "coverImage": coverImage,
           "gender": gender,
           "editor": editor,
@@ -92,94 +133,6 @@ function createBook(id){
             body: JSON.stringify(clientRequest)
         })
             .then(response => response.json())
-    });
-    alert("usuario cadastrado");
-
-
-
-}
-
-
-function login() {
-
-    var form = document.getElementById("login");
-    form === null || form === void 0 ? void 0 : form.addEventListener("submit",function (event) {
-        event.preventDefault();
-
-        const email = document.getElementById("emailLogin").value
-        const password = document.getElementById("passwordLogin").value
-
-        let url = 'http://localhost:15000/client/login?email=' + email + '&password=' + password;
-
-        fetch(url, {
-            method: 'GET',
-            headers: {
-                // 'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-        })
-            .then(response =>  response.json())
-            .then((json) => {
-                loggedClient = json;
-                window.open('./index.html');
-                window.close();
-            })
-    });
-
-    window.open('./index.html');
-    window.close();
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function create() {
-
-    let url = 'http://localhost:15000/client';
-
-    var form = document.getElementById("create");
-    form === null || form === void 0 ? void 0 : form.addEventListener("submit", function (event) {
-        event.preventDefault();
-
-        const userName = document.getElementById("userName").value
-        const email = document.getElementById("email").value
-        const password = document.getElementById("password").value
-
-        var clientRequest = {
-          "userName": userName,
-          "email": email,
-          "password": password,
-        };
-
-        fetch(url, {
-            method: 'POST',
-            headers: {
-                // 'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(clientRequest)
-        })
-            .then(response => response.json())
-    });
-    alert("usuario cadastrado");
+    });*/
+    alert("Livro cadastrado");
 }
